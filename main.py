a3dd60a9256ade419530805bc37170922d03d974
vorcarobelford-spec <vorcarobelford@gmail.com>
2026-05-25 17:13:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import google.generativeai as genai
+from google import genai
 import os
 
 app = FastAPI()
 
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-
-model = genai.GenerativeModel("gemini-1.5-flash")
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 class Msg(BaseModel):
     texto: str
@@ -19,7 +17,13 @@ def home():
 @app.post("/ia")
 def ia(msg: Msg):
     try:
-        response = model.generate_content(msg.texto)
+        response = client.models.generate_content(
+            model="gemini-1.5-flash",
+            contents=msg.texto
+        )
+
         return {"resposta": response.text}
+    
     except Exception as e:
         return {"erro": str(e)}
+        
